85e4efa561e3834ed8260c8ab1e6c04a82f1ae82
younc <1826340163@qq.com>
2024-02-04 12:46:11 +0800

    Update GitHub Actions and Nuxt.js configuration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 个人项目，请勿下载❗
+<div align="center"><img src="https://raw.githubusercontent.com/Achuan-2/Achuan-2/main/assets/github-contribution-grid-snake.svg" ></div>
+<div align="center"><img src="https://cdn.jsdelivr.net/gh/Achuan-2/Achuan-2/assets/github-contribution-grid-snake.svg" ></div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+console.log(process.env.DEPLOY_PRESET === "github-pages" ? process.env.DEPLOY_GITHUB_BASEURL : process.env.DEPLOY_BASEURL, process.env.DEPLOY_PRESET === "github-pages" ? "assets" : "/_nuxt/");
 
-export default defineNuxtConfig({
+export default  defineNuxtConfig({
   devtools: { enabled: false },
   css: ["~/assets/css/tailwind.css", "~/assets/css/global.css", "animate.css/animate.min.css"],
   modules: ["@nuxtjs/eslint-module", "nuxt-headlessui", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
@@ -14,5 +15,14 @@ export default defineNuxtConfig({
     fix: process.env.LINT_FIX === "true",
     include: ["./components", "./layouts", "./plugins", "./utils", "./pages", "./stores", "./server/**/*.ts"],
     lintOnStart: process.env.LINT_ON_START === "true"
+  },
+
+  app: {
+    baseURL: process.env.DEPLOY_PRESET === "github-pages" ? process.env.DEPLOY_GITHUB_BASEURL : process.env.DEPLOY_BASEURL,
+    buildAssetsDir: process.env.DEPLOY_PRESET === "github-pages" ? "assets" : "/_nuxt/"
+  },
+  nitro: {
+    preset: process.env.DEPLOY_PRESET,
+    baseURL: process.env.DEPLOY_PRESET === "github-pages" ? process.env.DEPLOY_GITHUB_BASEURL : process.env.DEPLOY_BASEURL
   }
 });
